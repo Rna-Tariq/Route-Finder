1090c1370e74bf7cbb06a3736a9eb1773b15423f
Rna341 <rna.tariq.aboouf@gmail.com>
2025-04-09 16:54:32 +0200

    Added a placeholder function to decode OSRM polyline format to coordinates

---
+++ front-end/src/utils/navigationHelpers.js
@@ -0,0 +1,8 @@
+export const decodePolyline = (encoded) => {
+    // This function would decode OSRM's polyline format to coordinates
+    // Implementation depends on the encoding format used by your routing API
+    // For OSRM, you might need the polyline library
+
+    // Placeholder implementation:
+    return [];
+};
