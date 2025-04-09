f940c0dd7c5e879fa2a86577fec4dd1f159f6045
Rna341 <rna.tariq.aboouf@gmail.com>
2025-04-09 16:53:06 +0200

    Fetched route data from OSRM API and adjusted durations based on transport mode

--- front-end/src/services/routing.js
+++ front-end/src/services/routing.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Speed factors by transport mode (in meters per second)
 const SPEED_FACTORS = {
     walking: 1.4,      // ~5 km/h
     cycling: 4.2,      // ~15 km/h
@@ -20,27 +19,20 @@ export const fetchRoute = async (originData, destinationData, transportMode) => 
             throw new Error('No route found between these locations.');
         }
 
-        // Adjust duration based on transport mode
         const route = { ...response.data };
         const speedFactor = SPEED_FACTORS[transportMode];
 
-        // Adjust durations for the whole route and each step
         route.routes.forEach(routeItem => {
             if (routeItem.duration) {
-                // OSRM already provides estimated duration, but we can adjust based on our factors
-                // This is just a simple adjustment, in reality it would be more complex
                 if (transportMode === 'driving') {
-                    // Add traffic factor (20% more time on average)
                     routeItem.duration *= 1.2;
                 }
             }
 
-            // Adjust leg and step durations
             routeItem.legs.forEach(leg => {
                 leg.steps.forEach(step => {
                     if (step.duration) {
                         if (transportMode === 'driving') {
-                            // Add traffic factor
                             step.duration *= 1.2;
                         }
                     }
