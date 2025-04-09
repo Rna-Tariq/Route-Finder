1ce6779e45b4c73b1e8f451173b0d1cb6c70c58b
Rna341 <rna.tariq.aboouf@gmail.com>
2025-04-09 16:55:24 +0200

    Entry point

---
+++ front-end/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import './styles/global.css'
+import { AuthProvider } from './contexts/AuthContext';
+// import './index.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
+  </React.StrictMode>
+);
