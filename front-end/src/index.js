c762a4eb2a26e86ab508bb143ef1be7e7ed4a25d
Rna341 <rna.tariq.aboouf@gmail.com>
2025-04-22 14:20:08 +0200

    Imported i18n

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,7 +5,7 @@ import store from './redux/store';
 import App from './App';
 import './styles/global.css'
 import { AuthProvider } from './contexts/AuthContext';
-// import './index.css';
+import './i18n'; 
 
 const root = createRoot(document.getElementById('root'));
 
